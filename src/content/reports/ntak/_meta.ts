59c6833a4fc4912ba601f911e86917a780b35425
Levente Otta <leventeotta@gmail.com>
2024-11-29 03:16:09 +0100

    Add NTAK requests and responses

---
+++ src/content/reports/ntak/_meta.ts
@@ -0,0 +1,9 @@
+const meta = {
+  reservation: "Reservation report",
+  checkin: "Checkin report",
+  checkout: "Checkout report",
+  "room-change": "Room change report",
+  "daily-closing": "Daily closing report",
+};
+
+export default meta;
